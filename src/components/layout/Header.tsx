174f29d75c0eba642bad8d22fe9d0ca7f96c85b7
DabAZ <dabaz@dabaz.me>
2024-04-24 06:34:33 +0800

    feat: add mobile header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 import { Logo } from "@/components/common/Logo";
 import { useScrollTop } from "@/hooks/use-scroll-top";
@@ -9,6 +11,7 @@ import { navigationItems } from "@/config/navigation-items";
 import { Button } from "@/components/ui/Button";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
   const scrolled = useScrollTop();
 
   return (
@@ -19,18 +22,73 @@ export default function Header() {
       )}
     >
       <div className="mx-auto flex max-w-6xl flex-col justify-between font-medium text-white sm:flex-row sm:items-center">
-        <Link
-          href="/"
-          className="flex items-center gap-2 group"
+        <div className="flex items-center justify-between">
+          <Link
+            href="/"
+            className="flex items-center gap-2 group z-[999]"
+          >
+            <Logo className="h-6 sm:h-8 w-auto group-hover:rotate-180 duration-500" />
+            <p className="text-xl sm:text-2xl font-bold tracking-tight duration-500">
+              DabAZ
+            </p>
+            <span className="sr-only">
+              DabAZ Home Page
+            </span>
+          </Link>
+          <button
+            type="button"
+            className="block p-2 text-3xl text-white sm:hidden"
+            aria-expanded={open}
+            onClick={() => setOpen(true)}
+          >
+            <Menu className="-mr-2" />
+            <span className="sr-only">
+              Open menu
+            </span>
+          </button>
+        </div>
+
+        {/* Mobile menu */}
+        <div
+          className={cn(
+            "ga-4 fixed bottom-0 left-0 right-0 top-0 z-40 flex flex-col items-end bg-[#070815] pr-4 pt-14 transition-transform duration-500 ease-in-out motion-reduce:transition-none sm:hidden",
+            open ? "translate-x-0" : "translate-x-[100%]",
+          )}
         >
-          <Logo className="h-6 sm:h-8 w-auto group-hover:rotate-180 duration-500" />
-          <p className="text-xl sm:text-2xl font-bold tracking-tight duration-500">
-            DabAZ
-          </p>
-          <span className="sr-only">
-            DabAZ Home Page
-          </span>
-        </Link>
+          <button
+            type="button"
+            className="fixed right-2 top-4 mb-4 block p-2 text-3xl text-white sm:hidden"
+            aria-expanded={open}
+            onClick={() => setOpen(false)}
+          >
+            <X />
+            <span className="sr-only">
+              Close menu
+            </span>
+          </button>
+
+          <div className="grid justify-items-end gap-8">
+            {navigationItems.map((item) => (
+              <Link
+                key={item.label}
+                href={item.href}
+                onClick={() => setOpen(false)}
+                className="block px-3 text-2xl first:mt-8 text-neutral-300 hover:text-white duration-300"
+              >
+                {item.label}
+              </Link>
+            ))}
+            <Link
+              href="mailto:dabaz@dabaz.me"
+              onClick={() => setOpen(false)}
+              className="block px-3 text-2xl first:mt-8 text-neutral-300 hover:text-white duration-300"
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+
+        {/* Desktop menu */}
         <ul className="hidden sm:flex gap-6 items-center">
         {navigationItems.map((item) => (
           <li key={item.label}>
