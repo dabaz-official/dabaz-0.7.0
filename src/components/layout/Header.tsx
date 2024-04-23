5665f310bc4387af5e89f6890e6070114da46b1d
DabAZ <dabaz@dabaz.me>
2024-04-24 04:53:59 +0800

    feat: add navigations in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,6 +6,7 @@ import { Logo } from "@/components/common/Logo";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { navigationItems } from "@/config/navigation-items";
+import { Button } from "@/components/ui/Button";
 
 export default function Header() {
   const scrolled = useScrollTop();
@@ -17,24 +18,36 @@ export default function Header() {
         "bg-transparent fixed top-0 left-0 right-0 z-[100] p-4 md:px-6", scrolled && "bg-black/80 border-b border-neutral-800 shadow-sm backdrop-blur-lg"
       )}
     >
-      <div className="mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center">
-        <Link href="/">
-          <Logo className="h-8 md:h-10 w-auto hover:rotate-180 duration-700" />
+      <div className="mx-auto flex max-w-6xl flex-col justify-between font-medium text-white md:flex-row md:items-center">
+        <Link
+          href="/"
+          className="flex items-center gap-2 group"
+        >
+          <Logo className="h-6 md:h-8 w-auto group-hover:rotate-180 duration-500" />
+          <p className="text-xl md:text-2xl font-bold tracking-tight">
+            DabAZ
+          </p>
           <span className="sr-only">
             DabAZ Home Page
           </span>
         </Link>
-        <ul className="flex gap-6">
-          {navigationItems.map((item) => (
-            <li key={item.label}>
-              <Link
-                href={item.href}
-                className="inline-flex min-h-11 items-center hover:text-neutral-200 transition"
-              >
-                {item.label}
-              </Link>
-            </li>
-          ))}
+        <ul className="flex gap-6 items-center">
+        {navigationItems.map((item) => (
+          <li key={item.label}>
+            <Button
+              variant="ghost"
+              href={item.href}
+            >
+              {item.label}
+            </Button>
+          </li>
+        ))}
+          <Button
+            variant="crazy"
+            href="mailto:dabaz@dabaz.me"
+          >
+            Contact
+          </Button>
         </ul>
       </div>
     </nav>
