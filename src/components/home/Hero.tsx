d5377b7e2d7470d17da23e8a1e7781906f92ea76
DabAZ <dabaz@dabaz.me>
2024-04-24 18:26:54 +0800

    refactor: add home.ts

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,32 +1,35 @@
 import Image from "next/image";
 
 import Bounded from "@/components/layout/Bounded";
-import GridBackground from "@/components/home/GridBackground";
-import homeTexts from "@/config/home-texts";
+import StarGrid from "@/components/home/StarGrid";
+import home from "@/config/home";
 
 export default function Hero() {
   return (
-    <Bounded>
-      <div className="relative">
-        <GridBackground />
-        <h1 className="text-balance text-center text-5xl font-medium md:text-7xl">
-          {homeTexts.hero.heading}
-        </h1>
-        <div className="mx-auto mt-6 max-w-lg text-center">
-          <p className="text-balance text-slate-300 text-xl md:text-2xl">
-            {homeTexts.hero.description}
-          </p>
+    <div>
+      <StarGrid />
+      <Bounded>
+        <div className="relative">
+          <h1 className="text-balance text-center text-5xl font-medium md:text-7xl">
+            {home.hero.heading}
+          </h1>
+          <div className="mx-auto mt-6 max-w-lg text-center">
+            <p className="text-balance text-slate-300 text-xl md:text-2xl">
+              {home.hero.description}
+            </p>
+          </div>
+          <div className="glass-container mt-16 w-96 mx-auto">
+            <div className="absolute inset-0 -z-10 bg-blue-500/30 blur-2xl filter" />
+            <Image
+              src={home.hero.imgSrc}
+              alt={home.hero.imgAlt}
+              width={1208}
+              height={1208}
+              className="rounded-2xl"
+            />
+          </div>
         </div>
-        <div className="glass-container mt-16 w-96 mx-auto">
-          <Image
-            src="/images/home/avatar.jpg"
-            alt="DabAZ is standing on a beach, posing."
-            width={1208}
-            height={1208}
-            className="rounded-2xl"
-          />
-        </div>
-      </div>
-    </Bounded>
+      </Bounded>
+    </div>
   );
 };
--- src/config/home.ts
+++ src/config/home.ts
@@ -1,8 +1,10 @@
-const homeTexts = {
+const home = {
   hero: {
     heading: "Hello, I’m DabAZ.",
     description: "I am an ethical hacker, entrepreneur, music producer, designer, and front-end developer.",
+    imgSrc: "/images/home/avatar.jpg",
+    imgAlt: "DabAZ is standing on a beach.",
   },
 };
 
-export default homeTexts;
+export default home;
