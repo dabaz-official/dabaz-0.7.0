7324c812488658eb2d6f73210eb9167837c20747
DabAZ <dabaz@dabaz.me>
2024-04-24 17:59:58 +0800

    feat: update hero section

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,10 +1,31 @@
+import Image from "next/image";
+
 import Bounded from "@/components/layout/Bounded";
+import GridBackground from "@/components/home/GridBackground";
+import homeTexts from "@/config/home-texts";
 
 export default function Hero() {
   return (
     <Bounded>
       <div className="relative">
-        
+        <GridBackground />
+        <h1 className="text-balance text-center text-5xl font-medium md:text-7xl">
+          {homeTexts.hero.heading}
+        </h1>
+        <div className="mx-auto mt-6 max-w-lg text-center">
+          <p className="text-balance text-neutral-300 text-xl md:text-2xl">
+            {homeTexts.hero.description}
+          </p>
+        </div>
+        <div className="glass-container mt-16 w-96 mx-auto">
+          <Image
+            src="/images/home/avatar.jpg"
+            alt="DabAZ is standing on a beach, posing."
+            width={1208}
+            height={1208}
+            className="rounded-2xl"
+          />
+        </div>
       </div>
     </Bounded>
   );
---
+++ src/config/home-texts.ts
@@ -0,0 +1,8 @@
+const homeTexts = {
+  hero: {
+    heading: "Hello, I’m DabAZ.",
+    description: "I am an ethical hacker, entrepreneur, music producer, designer, and front-end developer.",
+  },
+};
+
+export default homeTexts;
