3ca49bba181020eece2ab45cae0b0ecc02e0704e
DabAZ <dabaz@dabaz.me>
2024-04-25 04:31:53 +0800

    build: setup sanity

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 
 import { seo } from "@/config/seo";
+import Bounded from "@/components/layout/Bounded";
+import home from "@/config/home";
 
 export const metadata: Metadata = {
   title: seo.blog.title,
@@ -31,8 +33,17 @@ export const metadata: Metadata = {
 
 export default function BlogPage() {
   return (
-    <div>
-      
-    </div>
+    <Bounded>
+      <div className="relative">
+        <h1 className="text-balance text-center text-5xl font-medium md:text-7xl">
+          {home.blog.heading} {home.blog.heading2}{home.blog.heading3} 
+        </h1>
+        <div className="mx-auto mt-6 max-w-lg text-center">
+          <p className="text-balance text-slate-300 text-xl md:text-2xl">
+            {home.blog.description}
+          </p>
+        </div>
+      </div>
+    </Bounded>
   );
 };
